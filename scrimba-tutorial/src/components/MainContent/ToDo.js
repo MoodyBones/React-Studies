42411af41759add3ce1d3a7f1e20f83aceda5a77
Mel <jones.mel@gmail.com>
2019-07-30 17:51:53 +0200

    Styling ToDo

--- scrimba-tutorial/src/components/MainContent/ToDo.js
+++ scrimba-tutorial/src/components/MainContent/ToDo.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import Welcome from './Welcome'
+import ToDoItem from './ToDoItem'
 
 function ToDo() {
   return (
     <section className='toDo-wrapper'>
-      <Welcome />
-      <ul className='l-s-none'>
-        <li><input type='checkbox' /><span>Learn React</span></li>
-        <li><input type='checkbox' /><span>Complete Animation Tutorial</span></li>
-        <li><input type='checkbox' /><span>Review JS Fundamentals</span></li>
-        <li><input type='checkbox' /><span>Read up on a11y</span></li>
-      </ul>
+      <div className='toDo-list'>
+        {/* <Welcome /> */}
+        <ToDoItem />
+        <ToDoItem />
+        <ToDoItem />
+        <ToDoItem />
+        <ToDoItem />
+      </div>
+      
+      {/* <input type='checkbox' /><span>Learn React</span>
+      <input type='checkbox' /><span>Complete Animation Tutorial</span>
+      <input type='checkbox' /><span>Review JS Fundamentals</span>
+      <input type='checkbox' /><span>Read up on a11y</span> */}
     </section>
   )
 }
---
+++ scrimba-tutorial/src/components/MainContent/ToDoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ToDoItem() {
+  return (
+    <div className='toDo-item'>
+      <input type='checkbox' />
+      <p>Learn React</p>
+    </div>
+  )
+}
+
+export default ToDoItem
